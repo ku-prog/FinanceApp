be819bc4955f4d75e4c63d3f1ddaef8cabbc9731
ku-prog <148188363+ku-prog@users.noreply.github.com>
2024-11-05 18:23:31 +0100

    Add files via upload
    General description of basic app functionality

---
+++ FinanceApp_FreeMind.mm
@@ -0,0 +1,105 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1727979510268" ID="ID_1878686906" MODIFIED="1729179751636" TEXT="Menu G&#x142;&#xf3;wne">
+<node CREATED="1727979587566" ID="ID_1033991573" MODIFIED="1727979616095" POSITION="right" TEXT="1. Rejestracja">
+<node CREATED="1727979630659" ID="ID_1018111039" MODIFIED="1727979785629" TEXT="Podaj login">
+<node CREATED="1727979726702" ID="ID_773999994" MODIFIED="1727979731222" TEXT="Podaj has&#x142;o">
+<node CREATED="1727979731225" ID="ID_468793205" MODIFIED="1727979737597" TEXT="Powt&#xf3;rz has&#x142;o">
+<node CREATED="1727979737599" ID="ID_301895223" MODIFIED="1728029219677" TEXT="Utworzenie konta">
+<node CREATED="1728029176754" ID="ID_1521106381" MODIFIED="1728029210361" TEXT="Powr&#xf3;t do Menu G&#x142;&#xf3;wne"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1727979616589" ID="ID_1321639017" MODIFIED="1727979621193" POSITION="right" TEXT="2. Logowanie">
+<node CREATED="1727979950479" ID="ID_1836761014" MODIFIED="1727979956238" TEXT="Podaj login">
+<node CREATED="1727979956241" ID="ID_1574068320" MODIFIED="1728029291305" TEXT="Podaj has&#x142;o">
+<node CREATED="1727979968519" ID="ID_1944755083" MODIFIED="1727979979665" TEXT="Has&#x142;o poprawne - przej&#x15b;cie do menu U&#x17c;ytkownika">
+<node CREATED="1728029325768" ID="ID_436716439" MODIFIED="1728029356668" TEXT="Menu Zalogowanego U&#x17c;ytkownika">
+<node CREATED="1728029376402" ID="ID_810281534" MODIFIED="1729179572317" TEXT="1. Dodaj przych&#xf3;d">
+<node CREATED="1728029437479" ID="ID_712750969" MODIFIED="1728029463715" TEXT="Dotyczy dnia dzisiejszego?">
+<node CREATED="1728029484599" ID="ID_1432513994" MODIFIED="1730129327307" TEXT="Pobranie z systemu dzisiejszej daty i skorzystanie z niej">
+<icon BUILTIN="button_ok"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1729176036891" ID="ID_328941909" MODIFIED="1729176043921" TEXT="Czego dotyczy dany przych&#xf3;d?">
+<node CREATED="1729176053966" ID="ID_831900130" MODIFIED="1729176085591" TEXT="Okre&#x15b;l wysoko&#x15b;&#x107; przychodu (cz&#x119;&#x15b;ci dziesi&#x119;tne po kropce)"/>
+</node>
+</node>
+</node>
+<node CREATED="1728029465328" ID="ID_539995433" MODIFIED="1728029476907" TEXT="Dotyczy innego dnia?">
+<node CREATED="1728029508112" ID="ID_867526985" MODIFIED="1729175698029" TEXT="Wpisanie innej daty w formacie RRRR-MM-DD">
+<node CREATED="1728029599378" ID="ID_1799316857" MODIFIED="1729175608161" TEXT="Walidacja poprawno&#x15b;ci wprowadzonej daty">
+<node CREATED="1728029640016" ID="ID_497354130" MODIFIED="1728312982300" TEXT="Za&#x142;o&#x17c;enie: Data musi mie&#x15b;ci&#x107; si&#x119; w przedziale od 2000-01-01 do max ostatniego dnia bie&#x17c;&#x105;cego miesi&#x105;ca">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1728030428904" ID="ID_812746011" MODIFIED="1730239822468" TEXT="Prze&#x107;wiczy&#x107; funkcj&#x119; okre&#x15b;laj&#x105;c&#x105; ile dni ma dany miesi&#x105;c">
+<icon BUILTIN="full-2"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1728311063391" ID="ID_1024309467" MODIFIED="1728311466452" TEXT="Czego dotyczy dany przych&#xf3;d (np. wyp&#x142;ata, odsetki z lokat, sprzeda&#x17c; allegro)?">
+<node CREATED="1728311541670" ID="ID_718681344" MODIFIED="1728311594602" TEXT="Okre&#x15b;l wysoko&#x15b;&#x107; przychodu (cz&#x119;&#x15b;ci dziesi&#x119;tne zapisz po kropce. Je&#x15b;li u&#x17c;yto przecinka -&gt; zamie&#x144; na kropk&#x119;)"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1728029382287" ID="ID_1284744245" MODIFIED="1729179579183" TEXT="2. Dodaj wydatek">
+<node CREATED="1728311938937" ID="ID_650423679" MODIFIED="1729175621141" TEXT="Dotyczy dnia dzisiejszego?">
+<node CREATED="1729175641482" ID="ID_1476179918" MODIFIED="1729175657721" TEXT="Pobranie z systemu dzisiejszej daty i skorzystanie z niej">
+<node CREATED="1729176709155" ID="ID_246335200" MODIFIED="1729176718153" TEXT="Czego dotyczy wydatek?">
+<node CREATED="1729176718715" ID="ID_881329562" MODIFIED="1729176751178" TEXT="Okre&#x15b;l wysoko&#x15b;&#x107; wydatku (cz&#x119;&#x15b;ci dziesi&#x119;tne po kropce)"/>
+</node>
+</node>
+</node>
+<node CREATED="1729175623872" ID="ID_1398230933" MODIFIED="1729175631845" TEXT="Dotyczy innego dnia?">
+<node CREATED="1729175661260" ID="ID_558564472" MODIFIED="1729175680451" TEXT="Wpisanie innej daty w formacie RRRR-MM-DD">
+<node CREATED="1729175703961" ID="ID_1298795672" MODIFIED="1729175720034" TEXT="Walidacja poprawno&#x15b;ci wprowadzonej daty">
+<node CREATED="1729175721460" ID="ID_1358782691" MODIFIED="1729175769321" TEXT="Za&#x142;o&#x17c;enie: Data musi mie&#x15b;ci&#x107; si&#x119; w przedziale od 2000-01-01 do max ostatniego dnia bie&#x17c;&#x105;cego miesi&#x105;ca">
+<node CREATED="1729175771464" ID="ID_1196899507" MODIFIED="1729175844496" TEXT="Czego dotyczy dany wydatek (np. jedzenie, mieszkanie, transport)">
+<node CREATED="1729175862231" ID="ID_1383875402" MODIFIED="1729176013631" TEXT="Okre&#x15b;l wysoko&#x15b;&#x107; wydatku (cz&#x119;&#x15b;ci dziesi&#x119;tne po kropce. Ewentualny przecinek zamieni&#x107; na kropk&#x119;)"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1728029387655" ID="ID_362010901" MODIFIED="1729179584292" TEXT="3. Bilans z bie&#x17c;&#x105;cego miesi&#x105;ca">
+<node CREATED="1729176804283" ID="ID_1819304484" MODIFIED="1729176872863" TEXT="Wy&#x15b;wietl przychody, a nast&#x119;pnie wydatki">
+<node CREATED="1729176880179" ID="ID_1306483525" MODIFIED="1729176918198" TEXT="Posegreguj pozycje wzgl&#x119;dem daty dodania - na g&#xf3;rze najstarsze.">
+<node CREATED="1729177035729" ID="ID_377909869" MODIFIED="1729177062167" TEXT="Na dole suma przychod&#xf3;w oraz suma wydatk&#xf3;w">
+<node CREATED="1729177103107" ID="ID_589557946" MODIFIED="1729177116198" TEXT="Podaj r&#xf3;&#x17c;nic&#x119; przychodzy - wydatki"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1728029399799" ID="ID_238154339" MODIFIED="1729179590035" TEXT="4. Bilans z poprzedniego miesi&#x105;ca">
+<node CREATED="1729178010252" ID="ID_358679290" MODIFIED="1729178025738" TEXT="Tak samo jak dla bie&#x17c;&#x105;cego miesi&#x105;ca"/>
+</node>
+<node CREATED="1728029409047" ID="ID_1326551509" MODIFIED="1729179595196" TEXT="5. Bilans  z wybranego okresu">
+<node CREATED="1729178030182" ID="ID_286592171" MODIFIED="1729178303512" TEXT="Popro&#x15b; U&#x17c;ytkownika podanie zakresu dat od kiedy do kiedy w formacie RRRR-MM-DD">
+<node CREATED="1729178317459" ID="ID_1549414867" MODIFIED="1729178324392" TEXT="Wypisz dane jak powy&#x17c;ej"/>
+</node>
+</node>
+<node CREATED="1728029416575" ID="ID_282170880" MODIFIED="1729179602522" TEXT="6. Zmie&#x144; has&#x142;o"/>
+<node CREATED="1728029421159" ID="ID_1525951399" MODIFIED="1729179609117" TEXT="7. Wyloguj si&#x119;"/>
+</node>
+</node>
+<node CREATED="1727979989049" ID="ID_1931675571" MODIFIED="1727980013234" TEXT="Has&#x142;o niepoprawne - wprowad&#x17a; ponownie (max 3 pr&#xf3;by)"/>
+</node>
+</node>
+</node>
+<node CREATED="1727979621854" ID="ID_1618940474" MODIFIED="1727979627328" POSITION="right" TEXT="3. Wyjscie"/>
+<node CREATED="1729179753436" ID="ID_267215729" MODIFIED="1729498408368" POSITION="left" TEXT="Wykorzystanie plik&#xf3;w XML">
+<node CREATED="1729179773039" ID="ID_718548765" MODIFIED="1729179782053" TEXT="users.xml">
+<node CREATED="1729179787208" ID="ID_1927530922" MODIFIED="1729179818659" TEXT="Zapisywane dane: userId, login, password, name, surname"/>
+</node>
+<node CREATED="1729179824459" ID="ID_1117273200" MODIFIED="1729179834263" TEXT="incomes.xml">
+<node CREATED="1729179839712" ID="ID_844911813" MODIFIED="1729179873604" TEXT="Zapisywane dane: incomeId, userId, date, item, amount"/>
+</node>
+<node CREATED="1729179886851" ID="ID_356578239" MODIFIED="1729179899984" TEXT="expenses.xml">
+<node CREATED="1729179900423" ID="ID_922220307" MODIFIED="1729179929811" TEXT="Zapisywane dane: expenseId, userId, date, item, amount"/>
+</node>
+</node>
+</node>
+</map>
